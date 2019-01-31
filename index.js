47dc9f994c0367901e429843a03cb8303e50228a
Nadir Sarigul <nadir.nyc62@gmail.com>
2019-01-31 03:28:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ function theBeatlesPlay(musicians, instruments){
   for(var i = 0; i<musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
   }
-  return array
+  returnarray
 
+}
+function johnLennonFacts(facts){
+  var results =[]
+  var count = 0
+  while(count<facts.length){
+    results.push(facts[count]+ "!!!")
+    count++
+  }
+  return results
 }
